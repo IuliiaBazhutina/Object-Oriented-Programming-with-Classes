e49c45e02e1b7019262e62a7bcb985a4bf3106e6
Iuliia Bazhutina <Iuliia.Bazhutina@gmail.com>
2024-12-11 14:16:43 -0500

    added 2 instances

--- script.js
+++ script.js
@@ -21,6 +21,14 @@ class PerishableProductProperties extends ProductProperties {
         this.expirationDate = expirationDate;
     }
 
+    toString() {
+        return `Product: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}, Expiration date: ${this.expirationDate}`
+    }
+
 }
 
+const milk = new PerishableProductProperties("Milk 1L", 5.5, 100, "2024-12-31");
+console.log(milk.toString());
+const cream = new PerishableProductProperties("Cream 0.5L", 3.5, 50, "2025-01-31");
+console.log(cream.toString());
 
