a601bba244598e61774eaf33bb75f3cddc77f595
Iuliia Bazhutina <Iuliia.Bazhutina@gmail.com>
2024-12-11 17:15:13 -0500

    added a new product sugar

--- script.js
+++ script.js
@@ -62,3 +62,4 @@ ProductProperties.classProperties = [milk, cream];
 const eggs = new PerishableProductPropertiesroductProperties("Eggs 1dozen", 4.5, 120, "2024-12-25");
 const butter = new PerishableProductPropertiesroductProperties("Butter 250gr", 8, 90, "2025-01-15");
 const salt = new ProductProperties ("Salt 1kg", 3.25, 200);
+const sugar = new ProductProperties("Sugar 1kg, 4.25, 150");
