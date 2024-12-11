d005635f852a92d9802ad79444b7cc39c43b2412
Iuliia Bazhutina <Iuliia.Bazhutina@gmail.com>
2024-12-11 17:40:08 -0500

    added class PropertiesInventory

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 class ProductProperties {
-    static classProperties;
+    static classProperties = [];
 
     constructor(name, price, quantity) {
         this.name = name;
@@ -20,23 +20,6 @@ class ProductProperties {
             product.price = product.price * (1 - discount);
         });
     }
-
-    addProduct(product) {
-        classProperties.push(product);
-        }
-
-    getInventoryValue() {
-        let totalValue = 0;
-        classProperties.forEach(product => {
-            totalValue += product.price * product.quantity;
-        })
-        return totalValue;
-    }
-
-    findProductByName(name) {
-        const object = classProperties.find(product => product.name === name);
-        return object;
-    }
 }
 
 
@@ -52,15 +35,47 @@ class PerishableProductProperties extends ProductProperties {
 
 }
 
+class PropertiesInventory {
+    constructor(inventory){
+        this.inventory = inventory;
+    }
+
+    addProduct(product) {
+        this.inventory.push(product);
+        }
+
+    getInventoryValue() {
+        let totalValue = 0;
+        this.inventory.forEach(product => {
+            totalValue += product.price * product.quantity;
+        })
+        return totalValue;
+    }
+
+    static findProductByName(name) {
+        const object = this.inventory.find(product => product.name === name);
+        return object;
+    }
+}
+
 const milk = new PerishableProductProperties("Milk 1L", 5.5, 100, "2024-12-31");
 console.log(milk.toString());
 const cream = new PerishableProductProperties("Cream 0.5L", 3.5, 50, "2025-01-31");
 console.log(cream.toString());
 
-ProductProperties.classProperties = [milk, cream];
+ProductProperties.classProperties.push(milk);
+ProductProperties.classProperties.push(cream); 
 
-const eggs = new PerishableProductPropertiesroductProperties("Eggs 1dozen", 4.5, 120, "2024-12-25");
-const butter = new PerishableProductPropertiesroductProperties("Butter 250gr", 8, 90, "2025-01-15");
+const eggs = new PerishableProductProperties("Eggs 1dozen", 4.5, 120, "2024-12-25");
+const butter = new PerishableProductProperties("Butter 250gr", 8, 90, "2025-01-15");
 const salt = new ProductProperties ("Salt 1kg", 3.25, 200);
 const sugar = new ProductProperties("Sugar 1kg", 4.25, 150);
 const flour = new ProductProperties("Flour 1kg", 4.75, 125);
+
+ProductProperties.addProduct(eggs);
+ProductProperties.addProduct(butter);
+ProductProperties.addProduct(salt);
+ProductProperties.addProduct(sugar);
+ProductProperties.addProduct(flour);
+
+console.log(ProductProperties.classProperties);
